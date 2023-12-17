7bc4739c01c68b1d1521ed708a03e2d039198735
Viktor Nordling <viktor@Bjorns-MacBook-Air.local>
2023-12-17 17:32:58 +1100

    Day 8 part 1 done.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fs;
 use std::ops::Index;
-use std::rc::Rc;
-
-mod day7;
 
 #[derive(Eq, PartialEq, Debug)]
 struct Node {
     name: String,
-    left: Option<Rc<RefCell<Node>>>,
-    right: Option<Rc<RefCell<Node>>>,
+    left: String,
+    right: String,
 }
 
 impl Node {
-    fn new(name: &String) -> Self {
+    fn new(name: &String, left: &String, right: &String) -> Self {
         Node {
             name: name.to_owned(),
-            left: None,
-            right: None,
+            left: left.to_owned(),
+            right: right.to_owned(),
         }
     }
 }
 
 fn main() {
     // Part 1.
-    let file_path = "/Users/vnordling/RustroverProjects/advent/src/input8.txt";
+    let file_path = "/Users/viktor/sources/adventofcode2023/src/input8.txt";
 
     let lines: Vec<String> = fs::read_to_string(file_path)
         .unwrap()
@@ -33,7 +29,7 @@ fn main() {
         .map(String::from)
         .collect();
 
-    let mut node_map: HashMap<String, Rc<RefCell<Node>>> = HashMap::new();
+    let mut node_map: HashMap<String, Node> = HashMap::new();
 
     for line in lines.iter().skip(2) {
         let parts: Vec<&str> = line.split("=").collect();
@@ -42,21 +38,9 @@ fn main() {
         let left = lr[0].index(2..);
         let right = lr[1].index(..lr[1].len() - 1);
 
-        let current_node= node_map.entry(node_name.to_string())
-            .or_insert(Rc::new(RefCell::new(Node::new(&node_name.to_string())))).clone();
-
-        let left_node= node_map.entry(left.to_string())
-            .or_insert(Rc::new(RefCell::new(Node::new(&left.to_string())))).clone();
-
-        let right_node= node_map.entry(right.to_string())
-            .or_insert(Rc::new(RefCell::new(Node::new(&right.to_string())))).clone();
-
-
-        current_node.borrow_mut().left = Some(left_node.clone());
-        current_node.borrow_mut().right = Some(right_node.clone());
+        let node = Node::new(&node_name.to_string(), &left.to_string(), &right.to_string());
+        node_map.insert(node_name.to_string(), node);
     }
-
-    // let dirs = &lines[0];
 
     // let left = &a.borrow().left.clone().unwrap();
     // let right = &a.borrow().right.clone().unwrap();
@@ -64,23 +48,23 @@ fn main() {
     // println!("Right: {}", right.borrow().name);
 
     let mut idx = 0;
-    let mut a = node_map.get("AAA").unwrap().borrow_mut();
+    let mut a = node_map.get("AAA").unwrap();
     let dirs: Vec<char> = lines[0].chars().collect();
+    let mut count = 0;
     while a.name != "ZZZ" {
         let dir = dirs.get(idx);
         if dir.unwrap() == &'L' {
-            let aa = a.left.as_ref().unwrap().clone();
-            a = aa.borrow_mut();
+            a = node_map.get(&a.left).unwrap();
         } else {
-            a = a.right.as_ref().unwrap().clone().borrow_mut();
+            a = node_map.get(&a.right).unwrap();
         }
         idx += 1;
+        count += 1;
         if idx >= dirs.len() {
             idx = 0;
         }
     }
 
-    let sum = 0;
-    println!("Part 1: {}", sum);
+    println!("Part 1: {}", count);
 
 }
