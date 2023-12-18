7cae16df408192c9e33b60f6b341979c787cbff6
Viktor Nordling <vnordling@anduril.com>
2023-12-18 14:15:30 +1100

    Day 9

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-mod day8;
-
-use std::collections::HashMap;
 use std::fs;
-use std::ops::Index;
-use num::Integer;
 
 fn find_next(line: &String) -> i32 {
     let nums: Vec<i32> = line.split_whitespace()
@@ -11,7 +6,8 @@ fn find_next(line: &String) -> i32 {
         .collect();
     let mut diffs: Vec<Vec<i32>> = Vec::new();
     let mut cur_diff = nums.clone();
-    while !cur_diff.iter().all(|x| *x == 0) {
+    // let aa = cur_diff.iter().all(|x| *x == 0);
+    while !all_zero(&cur_diff) {
         let mut new_diffs: Vec<i32> = Vec::new();
         for i in 0..(cur_diff.len() - 1) {
             new_diffs.push(cur_diff[i + 1] - cur_diff[i])
@@ -19,12 +15,52 @@ fn find_next(line: &String) -> i32 {
         cur_diff = new_diffs.clone();
         diffs.push(cur_diff.clone());
     }
-    return 0
+    let mut num = 0;
+    for i in 0..diffs.len() - 1 {
+        let cc = diffs.get(diffs.len() - i - 2).unwrap();
+        println!("cc.last = {}, num = {}", cc.last().unwrap(), num);
+        num = cc.last().unwrap() + num;
+        println!("new num = {}", num);
+    }
+    println!("num = {}", num + nums.last().unwrap());
+    return num + nums.last().unwrap();
+}
+
+fn find_prev(line: &String) -> i32 {
+    let nums: Vec<i32> = line.split_whitespace()
+        .filter_map(|word| word.parse().ok())
+        .collect();
+    let mut diffs: Vec<Vec<i32>> = Vec::new();
+    let mut cur_diff = nums.clone();
+    // let aa = cur_diff.iter().all(|x| *x == 0);
+    while !all_zero(&cur_diff) {
+        let mut new_diffs: Vec<i32> = Vec::new();
+        for i in 0..(cur_diff.len() - 1) {
+            new_diffs.push(cur_diff[i + 1] - cur_diff[i])
+        }
+        cur_diff = new_diffs.clone();
+        diffs.push(cur_diff.clone());
+    }
+    let mut num = 0;
+    for i in 0..diffs.len() - 1 {
+        let cc = diffs.get(diffs.len() - i - 2).unwrap();
+        println!("cc.last = {}, num = {}", cc.last().unwrap(), num);
+        num = cc.first().unwrap() - num;
+        println!("new num = {}", num);
+    }
+    println!("num = {}", nums.first().unwrap() - num);
+    return nums.first().unwrap() - num;
+}
+
+fn all_zero(cur_diff: &Vec<i32>) -> bool {
+    let all_zero = cur_diff.iter().all(|x| *x == 0);
+    println!("All zero? {}", all_zero);
+    return all_zero;
 }
 
 fn main() {
     // Part 1.
-    let file_path = "/Users/viktor/sources/adventofcode2023/src/input9.txt";
+    let file_path = "/Users/vnordling/RustroverProjects/advent/src/input9.txt";
 
     let lines: Vec<String> = fs::read_to_string(file_path)
         .unwrap()
@@ -33,9 +69,16 @@ fn main() {
         .collect();
 
     let mut sum = 0;
-    for line in lines {
+    for line in lines.clone() {
         let next = find_next(&line);
         sum += next;
     }
     println!("Part 1: {}", sum);
+
+    let mut sum2 = 0;
+    for line in lines {
+        let prev = find_prev(&line);
+        sum2 += prev;
+    }
+    println!("Part 2: {}", sum2);
 }
