d9d0cf41035ee1aaa53271d1bd8d603d600e505f
Viktor Nordling <vnordling@anduril.com>
2023-12-27 21:42:42 +1100

    Day 14 part 1 done, working on part 2.

--- src/main.rs
+++ src/main.rs
@@ -4,135 +4,132 @@ use std::fs;
 
 fn main() {
     // Part 1.
-    let file_path = "/Users/vnordling/RustroverProjects/advent/src/input13.txt";
+    let file_path = "/Users/vnordling/RustroverProjects/advent/src/input14.txt";
 
-    let input = fs::read_to_string(file_path).unwrap();
-    let cases: Vec<&str> = input.split("\n\n").collect();
+    let lines: Vec<String> = fs::read_to_string(file_path)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect();
 
-    let mut sum: usize = 0;
-    for case in &cases {
-        let lines: Vec<String> = case
-            .lines()
-            .map(String::from)
-            .collect();
-        let grid: Vec<Vec<char>> = lines.iter()
-            .map(|line| line.chars().collect())
-            .collect();
+    let grid: Vec<Vec<char>> = lines.iter()
+        .map(|line| line.chars().collect())
+        .collect();
 
-        let notes = find_reflection(&grid, None);
-        sum += notes.unwrap();
-    }
+    println!("Original grid:");
+    print_grid(&grid);
+    let new_grid: Vec<Vec<char>> = fall_east(&grid);
+    println!();
+    println!("East grid:");
+    print_grid(&new_grid);
+    // print_grid(&new_grid);
+    let score = count_score(&new_grid);
+    println!("Part 1: {}", score);
 
-    println!("Part 1: {}", sum);
-
-    sum = 0;
-    let mut case_id = 0;
-    for case in &cases {
-        println!("solving case: {}", case_id);
-        case_id += 1;
-        let lines: Vec<String> = case
-            .lines()
-            .map(String::from)
-            .collect();
-        let grid: Vec<Vec<char>> = lines.iter()
-            .map(|line| line.chars().collect())
-            .collect();
-        sum += solve_smudge(grid);
-    }
-    println!("Part 2: {}", sum);
+    // Do one cycle.
+    // let north_grid: Vec<Vec<char>> = fall_north(&grid);
+    // let west_grid: Vec<Vec<char>> = fall_west(&north_grid);
+    // let south_grid: Vec<Vec<char>> = fall_south(&west_grid);
+    // let east_grid: Vec<Vec<char>> = fall_east(&south_grid);
+    //
+    // println!("After 1 cycle: ");
+    // print_grid(&east_grid);
 }
 
-fn solve_smudge(grid: Vec<Vec<char>>) -> usize {
-    let unsmudged_solution = find_reflection(&grid, None).unwrap();
+fn print_grid(p0: &Vec<Vec<char>>) {
+    for y in 0..p0.len() {
+        for x in 0..p0[0].len() {
+            print!("{}", p0[y][x]);
+        }
+        println!();
+    }
+}
+
+fn count_score(grid: &Vec<Vec<char>>) -> usize {
+    let mut score = 0;
     for y in 0..grid.len() {
         for x in 0..grid[0].len() {
-            let mut new_grid = grid.clone();
-            // print!("Changing x = {}, y = {}", x, y);
-            if new_grid[y][x] == '#' {
-                // println!(" from # to .");
-                new_grid[y][x] = '.';
-            } else {
-                // println!(" from . to #");
-                new_grid[y][x] = '#';
-            }
-            let result = find_reflection(&new_grid, Option::from(unsmudged_solution));
-            if result.is_some() {
-                return result.unwrap();
+            if grid[y][x] == 'O' {
+                score += grid.len() - y;
             }
         }
     }
-    panic!("No reflection found!")
+    return score;
 }
 
-fn find_reflection(grid: &Vec<Vec<char>>, ignore: Option<usize>) -> Option<usize> {
-    for y in 0..grid.len() - 1 {
-        if lines_equal(&grid[y], &grid[y + 1]) {
-            println!("Try folding from y = {}", y);
-            let mut low = y as i32 - 1;
-            let mut high = (y + 2) as i32;
-            let mut reflection = true;
-            while low >= 0 && high < grid.len() as i32 {
-                if !lines_equal(&grid[low as usize], &grid[high as usize]) {
-                    reflection = false;
-                    break;
-                }
-                low -= 1;
-                high += 1;
-            }
-            if reflection {
-                // println!("Found reflection from row {}!", y);
-                let sol = (y + 1) * 100;
-                if ignore.is_some() && ignore.unwrap() == sol {
-                    // Ignoring this solution.
-                } else {
-                    return Option::from(sol);
-                }
-            }
-        }
-    }
-    let cols = grid[0].len();
-    for x in 0..cols - 1 {
-        if cols_equal(x, x + 1, &grid) {
-            // println!("Try folding from x = {}", x);
-            let mut low: i32 = x as i32 - 1;
-            let mut high: i32 = (x + 2) as i32;
-            let mut reflection = true;
-            while low >= 0 && high < cols as i32 {
-                if !cols_equal(low as usize, high as usize, &grid) {
-                    reflection = false;
-                    break;
-                }
-                low -= 1;
-                high += 1;
-            }
-            if reflection {
-                // println!("Found reflection from col {}!", x);
-                let sol = x + 1;
-                if ignore.is_some() && ignore.unwrap() == sol {
-                    // Ignoring this solution.
-                } else {
-                    return Option::from(sol);
-                }
-            }
-        }
-    }
-    return None;
-}
 
-fn cols_equal(col1: usize, col2: usize, grid: &Vec<Vec<char>>) -> bool {
+fn fall_north(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut fallen = grid.clone();
     for y in 0..grid.len() {
-        if grid[y][col1] != grid[y][col2] {
-            return false;
+        for x in 0..grid[0].len() {
+            if grid[y][x] == 'O' {
+                // Fall north.
+                let mut new_y = y as i32;
+                fallen[y][x] = '.';
+                while new_y >= 0 && fallen[new_y as usize][x] == '.' {
+                    new_y -= 1;
+                }
+                // println!("Rock in x = {}, y = {} fell from y = {} to y = {}", x, y, y, new_y + 1);
+                fallen[(new_y + 1) as usize][x] = 'O';
+            }
         }
     }
-    return true;
+    return fallen;
 }
 
-fn lines_equal(line1: &Vec<char>, line2: &Vec<char>) -> bool {
-    for x in 0..line1.len() {
-        if line1[x] != line2[x] {
-            return false;
+fn fall_south(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut fallen = grid.clone();
+    for y in (0..=grid.len()).rev() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] == 'O' {
+                // Fall south.
+                let mut new_y = y as i32;
+                fallen[y][x] = '.';
+                while new_y < grid.len() as i32 && fallen[new_y as usize][x] == '.' {
+                    new_y += 1;
+                }
+                // println!("Rock in x = {}, y = {} fell from y = {} to y = {}", x, y, y, new_y + 1);
+                fallen[(new_y - 1) as usize][x] = 'O';
+            }
         }
     }
-    return true;
+    return fallen;
+}
+
+fn fall_east(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut fallen = grid.clone();
+    for x in (0..grid[0].len()).rev() {
+        for y in 0..grid.len() {
+            if grid[y][x] == 'O' {
+                // Fall east.
+                let mut new_x = x as i32;
+                fallen[y][x] = '.';
+                while new_x < grid[0].len() as i32 && fallen[y][new_x as usize] == '.' {
+                    new_x += 1;
+                }
+                // println!("Rock in x = {}, y = {} fell from x = {} to x = {}", x, y, y, new_y + 1);
+                fallen[y][(new_x - 1) as usize] = 'O';
+            }
+        }
+    }
+    return fallen;
+}
+
+fn fall_west(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut fallen = grid.clone();
+    for x in 0..grid[0].len() {
+        for y in 0..grid.len() {
+            if grid[y][x] == 'O' {
+                // Fall west.
+                let mut new_x = x as i32;
+                fallen[y][x] = '.';
+                while new_x >= 0 && fallen[y][new_x as usize] == '.' {
+                    new_x -= 1;
+                }
+                // println!("Rock in x = {}, y = {} fell from x = {} to x = {}", x, y, y, new_y + 1);
+                fallen[y][(new_x + 1) as usize] = 'O';
+            }
+        }
+    }
+    return fallen;
 }
