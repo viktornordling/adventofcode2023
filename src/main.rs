6d3b1e95cb962b21eb001068fb1a719470b15c64
Viktor Nordling <vnordling@anduril.com>
2024-01-02 10:06:02 +1100

    Day 17 part 1 finally done :sweatsmile

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::cmp::{min, Ordering, Reverse};
+use std::cmp::{Ordering, Reverse};
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::fs;
 use std::hash::{Hash, Hasher};
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct Pos {
     x: i32,
@@ -28,20 +29,6 @@ fn main() {
     let grid: Vec<Vec<char>> = lines.iter()
         .map(|line| line.chars().collect())
         .collect();
-
-    // let right = Pos { x: 1, y: 0 };
-    // let mut seen = HashSet::new();
-    // let mut cur_path = Vec::new();
-    // let mut cache = HashMap::new();
-
-    let mut seen: HashSet<State> = HashSet::new();
-    let s = State { heat: 37, pos: Pos { x: 2, y: 1 }, dir: Pos { x: 1, y: 0 }, steps: 1 };
-    seen.insert(s);
-    let s2 = State { heat: 37, pos: Pos { x: 2, y: 1 }, dir: Pos { x: 1, y: 0 }, steps: 1 };
-    if seen.contains(&s2) {
-        println!("Contains!");
-    }
-
 
     let min_heat_loss = dijkstra(&grid);
     println!("Part 1: {}", min_heat_loss);
@@ -90,16 +77,18 @@ fn dijkstra(grid: &Vec<Vec<char>>) -> i32 {
 
     min_heap.push(Reverse(State { heat: 0, pos: Pos{ x: 0, y: 0}, dir: right, steps: 0}));
     min_heap.push(Reverse(State { heat: 0, pos: Pos{ x: 0, y: 0}, dir: down, steps: 0}));
-    let mut seen: HashSet<State> = HashSet::new();
+    let mut seen: HashSet<(Pos, i8, Pos)> = HashSet::new();
+    let bottom_right = Pos { x: (grid[0].len() - 1) as i32, y: (grid.len() - 1) as i32 };
 
     while min_heap.len() > 0 {
         let u = min_heap.pop().unwrap().0;
-        // println!("Popped state: {:?}", u);
-        if seen.contains(&u) {
-            // println!("already seen {:?}", u);
+        if u.pos.eq(&bottom_right) {
+            return u.heat;
+        }
+        if seen.contains(&(u.pos, u.steps, u.dir)) {
             continue;
         }
-        seen.insert(u);
+        seen.insert((u.pos, u.steps, u.dir));
         let last_dir = u.dir;
         let cur_moves_in_one_dir = u.steps;
         let cur_pos = u.pos;
@@ -142,103 +131,7 @@ fn dijkstra(grid: &Vec<Vec<char>>) -> i32 {
             }
         }
     }
-    return dist.get(&Pos{ x: grid[0].len() as i32, y: grid.len() as i32 }).unwrap().clone();
-}
-
-fn _dfs(
-    cur_pos: Pos,
-    last_dir: Pos,
-    cur_moves_in_one_dir: i8,
-    heat_loss: i32,
-    grid: &Vec<Vec<char>>,
-    seen: &mut HashSet<Pos>,
-    cur_path: &mut Vec<(Pos, Pos)>,
-    cache: &mut HashMap<(Pos, Pos, i8), (i32, i32)>
-) -> i32 {
-    if cache.contains_key(&(cur_pos, last_dir, cur_moves_in_one_dir)) {
-        let cached_loss = cache.get(&(cur_pos, last_dir, cur_moves_in_one_dir)).unwrap().clone();
-        if cached_loss.0 < heat_loss {
-            return cached_loss.1;
-        }
-    }
-    let up = Pos { x: 0, y: -1 };
-    let down = Pos { x: 0, y: 1 };
-    let left = Pos { x: -1, y: 0 };
-    let right = Pos { x: 1, y: 0 };
-
-    let _dir_chars = HashMap::from([
-        (up, '^'),
-        (down, 'v'),
-        (left, '<'),
-        (right, '>'),
-    ]);
-
-    // println!("Current pos: {:?}, heat_loss: {}, cur_moves_in_one_dir: {}", cur_pos, heat_loss, cur_moves_in_one_dir);
-    if cur_pos.x == grid[0].len() as i32 - 1 && grid.len() as i32 - 1 == cur_pos.y {
-        // println!("Reached the goal. Path is: {:?}", cur_path);
-        // let mut map = grid.clone();
-        // for pos in cur_path {
-        //     let char = dir_chars.get(&pos.1).unwrap();
-        //     map[pos.0.y as usize][pos.0.x as usize] = char.clone();
-        // }
-        // println!("Reached the goal with heat loss {}", heat_loss);
-        // print_grid(&map);
-        // println!();
-        return heat_loss;
-    }
-
-    let dirs = vec![up, down, left, right];
-
-    let mut lowest_heat_loss = 999999;
-
-    for dir in dirs {
-        if dir == last_dir && cur_moves_in_one_dir == 3 {
-            continue;
-        }
-        if dir == up && last_dir == down {
-            continue;
-        }
-        if dir == down && last_dir == up {
-            continue;
-        }
-        if dir == left && last_dir == right {
-            continue;
-        }
-        if dir == right && last_dir == left {
-            continue;
-        }
-        let new_pos = Pos { x: cur_pos.x + dir.x, y: cur_pos.y + dir.y};
-        if seen.contains(&new_pos) {
-            continue;
-        }
-        if new_pos.x < 0 || new_pos.x >= grid[0].len() as i32 {
-            continue;
-        }
-        if new_pos.y < 0 || new_pos.y >= grid.len() as i32 {
-            continue;
-        }
-        let new_moves_in_dir = if dir == last_dir {
-            cur_moves_in_one_dir + 1
-        } else {
-            1
-        };
-        let char = grid[new_pos.y as usize][new_pos.x as usize];
-        let loss: i32 = char.to_string().parse().ok().unwrap();
-        cur_path.push((new_pos, dir));
-        seen.insert(cur_pos);
-        // if cur_pos.y == 3 && cur_pos.x == 1 {
-        //     println!("Trying to move {:?}", dir);
-        // }
-        let rest_heat_loss = _dfs(new_pos, dir, new_moves_in_dir, heat_loss + loss, grid, seen, cur_path, cache);
-        // if cur_pos.y == 3 && cur_pos.x == 1 {
-        //     println!("Result of dfs: {}", rest_heat_loss);
-        // }
-        seen.remove(&cur_pos);
-        cur_path.remove(cur_path.len() - 1);
-        lowest_heat_loss = min(lowest_heat_loss, rest_heat_loss);
-    }
-    cache.insert((cur_pos, last_dir, cur_moves_in_one_dir), (heat_loss, lowest_heat_loss));
-    return lowest_heat_loss;
+    panic!("Should not get here!");
 }
 
 fn _print_grid(grid: &Vec<Vec<char>>) {
