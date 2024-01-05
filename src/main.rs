f3b895656fe03a56ef675027fbcffe0e11ccb147
Viktor Nordling <vnordling@anduril.com>
2024-01-05 23:26:33 +1100

    Day 19 done.

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,323 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp::{max, min};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use std::fs;
-use std::hash::{Hash, Hasher};
 use std::ops::Index;
 
-use num::abs;
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-struct Pos {
-    x: i32,
-    y: i32,
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Rule {
+    var: Option<String>,
+    cmp_char: char,
+    cmp_nr: i32,
+    accept_all: bool,
+    reject_all: bool,
+    next_workflow: Option<String>,
 }
 
-impl Hash for Pos {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.x.hash(state);
-        self.y.hash(state);
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Part {
+    vars: HashMap<String, i32>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Workflow {
+    name: String,
+    rules: Vec<Rule>,
+}
+
+fn parse_workflow(str: &String) -> Workflow {
+    let workflow_name = str.index(0..str.find("{").unwrap());
+    let rules = str.index(str.find("{").unwrap() + 1..str.find("}").unwrap());
+    let parts: Vec<&str> = rules.split(",").collect();
+    let mut rule_vec: Vec<Rule> = Vec::new();
+    for part in parts {
+        if part.contains(':') {
+            let rule_parts: Vec<&str> = part.split(":").collect();
+            let part_1 = rule_parts[0];
+            let mut cmp_char = '<';
+            let pp: Vec<&str> = if part_1.contains('<') {
+                part_1.split('<').collect()
+            } else {
+                cmp_char = '>';
+                part_1.split('>').collect()
+            };
+            let cmp_nr: i32 = pp.get(1).unwrap().parse().ok().unwrap();
+            let var = pp.get(0).unwrap();
+            let next = rule_parts[1];
+            rule_vec.push(Rule { var: Option::from(var.to_string()), cmp_char, cmp_nr, accept_all: false, reject_all: false, next_workflow: Option::from(next.to_string()) })
+        } else if part == "R" {
+            rule_vec.push(Rule { var: None, cmp_char: '_', cmp_nr: 0, accept_all: false, reject_all: true, next_workflow: None })
+        } else if part == "A" {
+            rule_vec.push(Rule { var: None, cmp_char: '_', cmp_nr: 0, accept_all: true, reject_all: false, next_workflow: None })
+        } else {
+            // This should just be the name of the next workflow
+            rule_vec.push(Rule { var: None, cmp_char: '_', cmp_nr: 0, accept_all: false, reject_all: false, next_workflow: Option::from(part.to_string()) })
+        }
     }
+    return Workflow { name: workflow_name.to_string(), rules: rule_vec };
 }
 
 fn main() {
     // Part 1.
-    let file_path = "/Users/vnordling/RustroverProjects/advent/src/input18.txt";
+    let file_path = "/Users/vnordling/RustroverProjects/advent/src/input19.txt";
 
-    let lines: Vec<String> = fs::read_to_string(file_path)
-        .unwrap()
+    let input = fs::read_to_string(file_path).unwrap();
+    let parts: Vec<&str> = input.split("\n\n").collect();
+
+    let rule_part: &str = &parts[0];
+    let parts_part: &str = &parts[1];
+    let workflows: Vec<Workflow> = rule_part
         .lines()
         .map(String::from)
+        .map(|s| parse_workflow(&s))
         .collect();
-    let mut map: HashMap<Pos, char> = HashMap::new();
 
-    let mut cur_pos = Pos { x: 0, y: 0 };
+    let parts2: Vec<Part> = parts_part
+        .lines()
+        .map(String::from)
+        .map(|s| parse_part(&s))
+        .collect();
 
-    let up = Pos { x: 0, y: -1 };
-    let down = Pos { x: 0, y: 1 };
-    let left = Pos { x: -1, y: 0 };
-    let right = Pos { x: 1, y: 0 };
+    let sum: i32 = parts2.iter().filter(|p| apply_workflows(&p, &workflows)).map(|p| get_val(&p)).sum();
+    println!("Part 1: {}", sum);
 
-    let dir_map: HashMap<String, Pos> = HashMap::from([
-        ("D".to_string(), down),
-        ("R".to_string(), right),
-        ("U".to_string(), up),
-        ("L".to_string(), left),
+    // let max_nr = 1353;
+    // let min_nr = 1349;
+    //
+    // let mut accepted = 0;
+    //
+    // for x in min_nr..=max_nr {
+    //     for m in min_nr..=max_nr {
+    //         for a in min_nr..=max_nr {
+    //             for s in min_nr..=max_nr {
+    //                 let part: Part = create_part(x, m, a, s);
+    //                 if apply_workflows(&part, &workflows) {
+    //                     // println!("x = {}, m = {}, a = {}, s = {}, Accepted", x, m, a, s);
+    //                     accepted += 1;
+    //                 } else {
+    //                     // println!("x = {}, m = {}, a = {}, s = {}, Rejected", x, m, a, s);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // println!("Num accepted: {}", accepted);
+    // let wf_map: HashMap<&String, &Workflow> = workflows.into_iter().map(|w| (&w.name, w)).collect();
+    let wf_map: HashMap<&String, &Workflow> = this_function_should_not_be_needed(&workflows);
+    let bounds = Bounds::new();
+    let mut seen: HashSet<String> = HashSet::new();
+    let bounds = dfs(&wf_map, wf_map.get(&"in".to_string()).unwrap(), &bounds, &mut seen);
+    // println!("Found {} bounds", bounds.len());
+    let mut combos: i64 = 0;
+    for bound in &bounds {
+        let map = &bound.bounds;
+        let mut product: i64 = 1;
+        for bound in map {
+            let interval: i64 = max(0, bound.1.upper as i64 - bound.1.lower as i64 + 1);
+            // println!("{} interval: {}", bound.0, interval);
+            product *= interval;
+            // println!("prod = {}", product);
+        }
+        // if product > 0 {
+        //     println!("Bounds: {:?}", &bound);
+        // }
+        combos += product;
+    }
+    println!("Part 2: {}", combos);
+}
+
+fn this_function_should_not_be_needed(workflows: &Vec<Workflow>) -> HashMap<&String, &Workflow> {
+    return workflows.into_iter().map(|w| (&w.name, w)).collect();
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Bound {
+    lower: i32,
+    upper: i32,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Bounds {
+    bounds: BTreeMap<String, Bound>,
+}
+
+impl Bounds {
+    fn new() -> Self {
+        let min_bound = 1;
+        let max_bound = 4000;
+        let x_bounds = Bound { lower: min_bound, upper: max_bound };
+        let m_bounds = Bound { lower: min_bound, upper: max_bound };
+        let a_bounds = Bound { lower: min_bound, upper: max_bound };
+        let s_bounds = Bound { lower: min_bound, upper: max_bound };
+
+        Bounds {
+            bounds: BTreeMap::from([
+                ("x".to_string(), x_bounds),
+                ("m".to_string(), m_bounds),
+                ("a".to_string(), a_bounds),
+                ("s".to_string(), s_bounds),
+            ])
+        }
+    }
+}
+
+fn dfs(workflows: &HashMap<&String, &Workflow>, cur_workflow: &Workflow, bounds: &Bounds, seen: &mut HashSet<String>) -> Vec<Bounds> {
+    let mut result: Vec<Bounds> = Vec::new();
+    seen.insert(cur_workflow.name.to_string());
+    // println!("Cloning");
+    let mut cur_bounds = bounds.clone();
+    for rule in &cur_workflow.rules {
+        // println!("cur_bounds is now {:?}", cur_bounds);
+        if rule.accept_all {
+            // println!("Found accept ALL rule");
+            result.push(cur_bounds.clone());
+            // println!("bounds is now: {:?}", result);
+        } else if rule.var.is_some() {
+            let var = rule.var.as_ref().unwrap();
+            let cmp = rule.cmp_char;
+            let cmp_nr = rule.cmp_nr;
+            // println!("update bounds 1");
+            let new_bounds = update_bounds(&cur_bounds, var, cmp, cmp_nr, false);
+            if rule.next_workflow.as_ref().unwrap() == "R" {
+                let cmp_opposite = if rule.cmp_char == '>' {
+                    '<'
+                } else {
+                    '>'
+                };
+                // println!("update bounds 2");
+                cur_bounds = update_bounds(&cur_bounds, var, cmp_opposite, cmp_nr, true);
+                // println!("Next is reject. cur_bounds is now {:?}", cur_bounds);
+                continue;
+            } else if rule.next_workflow.as_ref().unwrap() == "A" {
+                // println!("Found accept rule");
+                result.push(new_bounds);
+                // println!("bounds is now: {:?}", result);
+                let cmp_opposite = if rule.cmp_char == '>' {
+                    '<'
+                } else {
+                    '>'
+                };
+                // println!("update bounds 3");
+                cur_bounds = update_bounds(&cur_bounds, var, cmp_opposite, cmp_nr, true);
+                continue;
+            }
+            let next_workflow = workflows.get(rule.next_workflow.as_ref().unwrap()).unwrap();
+            if !seen.contains(&next_workflow.name) {
+                let sub_result = dfs(workflows, next_workflow, &new_bounds, seen);
+                for sbounds in sub_result {
+                    result.push(sbounds);
+                }
+            }
+            // In order to proceed to the next rule, the given rule must not be met, so update the bounds to the other side of the bounds.
+            let cmp_opposite = if rule.cmp_char == '>' {
+                '<'
+            } else {
+                '>'
+            };
+            // println!("update bounds 4, before: {:?}", cur_bounds);
+            cur_bounds = update_bounds(&cur_bounds, var, cmp_opposite, cmp_nr, true);
+            // println!("update bounds 4, after: {:?}", cur_bounds);
+        } else if rule.reject_all {
+            // Do nothing.
+        } else {
+            // This should be a final else
+            let next_workflow = workflows.get(rule.next_workflow.as_ref().unwrap()).unwrap();
+            if !seen.contains(&next_workflow.name) {
+                let sub_result = dfs(workflows, next_workflow, &cur_bounds, seen);
+                for bounds in sub_result {
+                    result.push(bounds);
+                }
+            }
+        }
+    }
+    return result;
+}
+
+fn update_bounds(bounds: &Bounds, var: &String, comp: char, limit: i32, inclusive: bool) -> Bounds {
+    let mut map = bounds.bounds.clone();
+    let cur_bound = map.get(var).unwrap();
+    let mut append = 0;
+    if !inclusive {
+        if comp == '>' {
+            append = 1;
+        } else {
+            append = -1;
+        }
+    }
+    let new_bound = if comp == '>' {
+        Bound { lower: max(cur_bound.lower, limit + append), upper: cur_bound.upper }
+    } else {
+        Bound { lower: cur_bound.lower, upper: min(cur_bound.upper, limit + append) }
+    };
+    map.insert(var.to_string(), new_bound);
+    Bounds { bounds: map }
+}
+
+fn create_part(x: i32, m: i32, a: i32, s: i32) -> Part {
+    let map: HashMap<String, i32> = HashMap::from([
+        ("x".to_string(), x),
+        ("m".to_string(), m),
+        ("a".to_string(), a),
+        ("s".to_string(), s),
     ]);
-
-    let dir_map2: HashMap<i8, Pos> = HashMap::from([
-        (0, right),
-        (1, down),
-        (2, left),
-        (3, up),
-    ]);
-
-    let mut points: Vec<Pos> = Vec::new();
-    for line in &lines {
-        let parts: Vec<&str> = line.split(" ").collect();
-        let dir = &parts[0];
-        let steps: i32 = parts[1].parse().ok().unwrap();
-        let step = dir_map.get(&dir.to_string()).unwrap();
-        for _ in 0..steps {
-            cur_pos = Pos { x: cur_pos.x + step.x, y: cur_pos.y + step.y };
-            map.insert(cur_pos.clone(), '#');
-            points.push(cur_pos.clone());
-        }
-    }
-    // print_grid(&map);
-    let mut seen = HashSet::new();
-    flood_fill(&mut map, Pos { x: 4, y: 1 }, &mut seen);
-    print_grid(&map);
-    let area = calc_area(&points);
-    println!("Part 1: {}", map.len());
-    println!("Part 1 mathy: {}", area);
-
-    // let mut new_map: HashMap<Pos, char> = HashMap::new();
-    let mut points2: Vec<Pos> = Vec::new();
-    for line in lines {
-        let parts: Vec<&str> = line.split(" ").collect();
-        let hex = parts[2];
-        let instr = hex.index(2..hex.len() - 2);
-        let steps = i32::from_str_radix(instr, 16).unwrap();
-        let dir: i32 = hex.index(hex.len() - 2..hex.len() - 1).parse().ok().unwrap();
-        let step = dir_map2.get(&(dir as i8)).unwrap();
-        for _ in 0..steps {
-            cur_pos = Pos { x: cur_pos.x + step.x, y: cur_pos.y + step.y };
-            // map.insert(cur_pos.clone(), '#');
-            points2.push(cur_pos.clone());
-        }
-    }
-
-    println!("Part 2: {}", calc_area(&points2));
+    return Part { vars: map };
 }
 
-fn calc_area(points: &Vec<Pos>) -> i64 {
-    let mut points_with_loop = points.clone();
-    points_with_loop.push(points[0]);
-    let temp: i64 = points_with_loop.iter()
-        .zip(points_with_loop.iter().skip(1))
-        .map(|zipped| zipped.0.y as i64 * zipped.1.x as i64 - zipped.1.y as i64 * zipped.0.x as i64).sum();
-    let interior_area: f64 = temp as f64 / 2f64;
-    let num_interior_points = abs(interior_area) - 0.5 * points.len() as f64 + 1f64;
-    return num_interior_points as i64 + points.len() as i64;
+fn get_val(part: &Part) -> i32 {
+    return part.vars.values().sum();
 }
 
-fn flood_fill(map: &mut HashMap<Pos, char>, cur_pos: Pos, seen: &mut HashSet<Pos>) {
-    let up = Pos { x: 0, y: -1 };
-    let down = Pos { x: 0, y: 1 };
-    let left = Pos { x: -1, y: 0 };
-    let right = Pos { x: 1, y: 0 };
-
-    let dirs = &vec![up, down, left, right];
-
-    map.insert(cur_pos, '#');
-    seen.insert(cur_pos);
-
-    for dir in dirs {
-        let new_pos = Pos { x: cur_pos.x + dir.x, y: cur_pos.y + dir.y };
-        if !seen.contains(&new_pos) && !map.contains_key(&new_pos) {
-            flood_fill(map, new_pos, seen);
+fn apply_workflows(part: &Part, workflows: &Vec<Workflow>) -> bool {
+    let wf_map: HashMap<&String, &Workflow> = workflows.into_iter().map(|w| (&w.name, w)).collect();
+    let mut cur_wf: &Workflow = wf_map.get(&"in".to_string()).unwrap();
+    let mut done = false;
+    let mut accepted = false;
+    while !done {
+        for rule in &cur_wf.rules {
+            if rule.accept_all {
+                accepted = true;
+                done = true;
+                break;
+            } else if rule.reject_all {
+                done = true;
+                break;
+            } else if !rule.var.is_some() {
+                cur_wf = wf_map.get(&rule.next_workflow.as_ref().unwrap()).unwrap();
+                break;
+            } else {
+                let var = rule.var.as_ref().unwrap();
+                let val = part.vars.get(var).unwrap();
+                let applies = if rule.cmp_char == '>' && val > &rule.cmp_nr {
+                    true
+                } else if rule.cmp_char == '<' && val < &rule.cmp_nr {
+                    true
+                } else {
+                    false
+                };
+                if applies {
+                    let next = rule.next_workflow.as_ref().unwrap();
+                    if next == "A" {
+                        accepted = true;
+                        done = true;
+                        break;
+                    } else if next == "R" {
+                        done = true;
+                        break;
+                    }
+                    cur_wf = wf_map.get(next).unwrap();
+                    break;
+                }
+            }
         }
     }
+    return accepted;
 }
 
-fn print_grid(map: &HashMap<Pos, char>) {
-    let min_y = map.keys().min_by(|k1, k2| k1.y.cmp(&k2.y)).unwrap().y;
-    let max_y = map.keys().max_by(|k1, k2| k1.y.cmp(&k2.y)).unwrap().y;
-
-    let min_x = map.keys().min_by(|k1, k2| k1.x.cmp(&k2.x)).unwrap().x;
-    let max_x = map.keys().max_by(|k1, k2| k1.x.cmp(&k2.x)).unwrap().x;
-
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            let char = map.get(&Pos { y, x }).unwrap_or_else(|| &'.');
-            print!("{}", char);
-        }
-        println!();
+fn parse_part(part: &String) -> Part {
+    let parts: Vec<&str> = part.index(1..part.len() - 1).split(",").collect();
+    let mut map: HashMap<String, i32> = HashMap::new();
+    for p in parts {
+        let s: Vec<&str> = p.split("=").collect();
+        map.insert(s[0].to_string(), s[1].parse().ok().unwrap());
     }
+    return Part { vars: map };
 }
